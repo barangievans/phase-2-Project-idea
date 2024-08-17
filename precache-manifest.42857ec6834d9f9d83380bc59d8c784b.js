f549ae56b8f61904f9c22724b95038e1bc66433d
barangievans <barangievans@gmail.com>
2024-08-17 20:14:41 +0300

    Updates

---
+++ precache-manifest.42857ec6834d9f9d83380bc59d8c784b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e0b7152bd5e21853b6f708997a305e",
+    "url": "/dairy-cow-management/index.html"
+  },
+  {
+    "revision": "1c82c65029515f87180c",
+    "url": "/dairy-cow-management/static/css/main.ddb60fc0.chunk.css"
+  },
+  {
+    "revision": "f4cf38de8792b92d7351",
+    "url": "/dairy-cow-management/static/js/2.8db2f731.chunk.js"
+  },
+  {
+    "revision": "cbf92ed71fb4f2e94692ba67215d89b7",
+    "url": "/dairy-cow-management/static/js/2.8db2f731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c82c65029515f87180c",
+    "url": "/dairy-cow-management/static/js/main.b955bc2d.chunk.js"
+  },
+  {
+    "revision": "252ab0116b5c87874983",
+    "url": "/dairy-cow-management/static/js/runtime-main.91cbd1c8.js"
+  }
+]);
